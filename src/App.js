5949a351eaf8778a54482139306c2108c9f406da
Revathi Ravi <123773127+Revathi1511@users.noreply.github.com>
2023-10-26 14:31:39 -0400

    new App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './screens/Home';
+// import Home from './screens/Home';
 import Restaurant from './screens/restaurant';
 import CardComponent from './components/card';
 import About from './screens/about';
